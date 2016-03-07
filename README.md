3b7367c449e6d3d8e0cbb334ef49c58f10ac2597
Ivo Gaydajiev <igaydajiev@vmware.com>
2016-03-07 13:53:59 +0200

    Expose hibernate statistics bean

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# vro-monitoring" 
+Enables Hibernate Statistics JMX bean for vRealize Orchestrator 5.5.x
+
+How to use it :
+1. Copy the vro-database-statistics.jar file into '{vRO Instalation}/app-server/deploy/vco/WEB-INF/lib' folder
+2. Restart vRO Server
+3. Open jconsole and connect to vRO server JMX 
+4. Locate Hibernate->Statistics bean
+5. To enable gathering of statistic invoke setStatisticsEnabled(true) method
+
+For visualization of statistics data hibernate-jconsole plugin can be used. hibernate-jconsole plugin can be downloaded from (http://hibernate-jcons.sourceforge.net/)
+This plugin can be used for for visualizing the statistics info.
+For details see http://hibernate-jcons.sourceforge.net/usage.html
+
+
+java -Dhibernate.classpath={PATH_TO_FOLDER_HIBERNATE_JARS}/app-server/deploy/vco/WEB-INF/lib;./; -jar hibernate-jconsole-1.0.7.jar
---
+++ src/main/java/com/vmware/o11n/monitoring/db/HibernateStatisticsFactoryBean.java
@@ -0,0 +1,31 @@
+package com.vmware.o11n.monitoring.db;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
+import org.hibernate.stat.Statistics;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class HibernateStatisticsFactoryBean implements FactoryBean<Statistics> {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public Statistics getObject() throws Exception {
+      SessionFactory sessionFactory = ((HibernateEntityManagerFactory) entityManagerFactory).getSessionFactory();
+      return sessionFactory.getStatistics();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+      return Statistics.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+      return true;
+    }
+  }
